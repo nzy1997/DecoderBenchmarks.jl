cfbc975a4254e3d8c9444df660f91f2d102fe6bc
Documenter.jl <documenter@juliadocs.github.io>
2025-06-20 14:07:03 +0000

    build based on 65ceb4c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DecoderBenchmarks","page":"Home","title":"DecoderBenchmarks","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DecoderBenchmarks.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
